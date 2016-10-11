Fix the softmax error.

rnn_batch_sind.py
@@ -94,7 +94,8 @@ class DMN_batch:
                                     sequences = q_shuffled,
                                     outputs_info = [T.zeros_like(inp_dummy)])
         q_inp_shuffled = q_inp.dimshuffle(2,0,1)
-        q_inp_last = q_inp_shuffled[:,-1,:] #batch_size * dim
+        q_inp_last = q_inp_shuffled[:,-1,:].dimshuffle(1,0) #dim * batch_size
+        
 
         # Now, share the parameter with the input module.
         self.W_inp_emb_q = nn_utils.normal_param(std = 0.1, shape=(self.dim, self.dim))
@@ -136,16 +137,18 @@ class DMN_batch:
         #print results.shape.eval({self.input_var: np.random.rand(10,4,4096).astype('float32'),
         #    self.q_var: np.random.rand(10, 4096).astype('float32'), 
         #    self.answer_inp_var: np.random.rand(10, 18, 8001).astype('float32')}, on_unused_input='ignore')
-        results = results[1:-1,:,:] # get rid of the last token as well as the first one (image)
+        #results = results[:-1,:,:] # get rid of the last token as well as the first one (image)
         #print results.shape.eval({self.input_var: np.random.rand(10,4,4096).astype('float32'),
         #    self.q_var: np.random.rand(10, 4096).astype('float32'), 
         #    self.answer_inp_var: np.random.rand(10, 18, 8001).astype('float32')}, on_unused_input='ignore')
             
         # Now, we need to transform it to the probabilities.
 
-        prob,_ = theano.scan(fn = lambda x, w: T.dot(w, x), sequences = results, non_sequences = self.W_a )
+        prob_f,_ = theano.scan(fn = lambda x, w: T.dot(w, x), sequences = results, non_sequences = self.W_a )
+        prob = prob_f[:-1,:,:]
 
         prob_shuffled = prob.dimshuffle(2,0,1) # b * len * vocab
+        prob_f_shuffled = prob_f.dimshuffle(2,0,1)
 
 
         logging.info("prob shape.")
@@ -155,8 +158,13 @@ class DMN_batch:
 
         n = prob_shuffled.shape[0] * prob_shuffled.shape[1]
         prob_rhp = T.reshape(prob_shuffled, (n, prob_shuffled.shape[2]))
-        prob_sm = nn_utils.softmax(prob_rhp)
-        self.prediction = prob_sm
+        prob_sm = nn_utils.softmax_(prob_rhp)
+
+        n_f = prob_f_shuffled.shape[0] * prob_f_shuffled.shape[1]
+        prob_f_rhp = T.reshape(prob_f_shuffled, (n_f, prob_f_shuffled.shape[2]))
+
+        prob_f_sm = nn_utils.softmax_(prob_f_rhp)
+        self.prediction = T.reshape(prob_f_sm, (prob_f_shuffled.shape[0], prob_f_shuffled.shape[1], prob_f_shuffled.shape[2]))
 
         mask =  T.reshape(self.answer_mask, (n,))
         lbl = T.reshape(self.answer_var, (n,))
@@ -199,6 +207,9 @@ class DMN_batch:
         self.test_fn = theano.function(inputs=[self.q_var, self.answer_var, self.answer_mask, self.answer_inp_var],
                                        outputs=[self.prediction, self.loss])
         
+        print "==> compiling pred_fn"
+        self.pred_fn = theano.function(inputs=[self.q_var, self.answer_inp_var],
+                                       outputs=[self.prediction])
     
     
     def GRU_update(self, h, x, W_res_in, W_res_hid, b_res,
@@ -293,14 +304,13 @@ class DMN_batch:
         stories = split_story[start_index:start_index+self.batch_size]
         #    slids.append( random.choice(range(len(split_dict_story[sid]))))
 
-        max_inp_len = 0
         max_q_len = 1 # just be 1.
         max_ans_len = 0
         for sid in stories:
-            max_inp_len = max(max_inp_len, len(split_dict_story[sid])-1)
-            #max_q_len = max(max_q_len, split_dict_story[sid][slid][1])
+            t_ans_len = 0
             for slid in split_dict_story[sid]:
-                max_ans_len = max(max_ans_len, len(slid[-1][-1]))
+                t_ans_len += len(slid[-1][-1])
+            max_ans_len = max(max_ans_len, t_ans_len)
         
         max_ans_len += 1 # this is for the start token.
         # in our case, it is pretty similar to the word-level dmn,
@@ -317,9 +327,8 @@ class DMN_batch:
                 anno = split_dict_story[sid]
                 question = []
                 answer = []
-                answer_mask = []
-                answer_inp = []
-
+                
+                answer.append(self.vocab_size)
                 for slid in split_dict_story[sid]:
                     input_anno = slid
                     img_id = input_anno[1][0]
@@ -330,54 +339,39 @@ class DMN_batch:
                     fc_fea = caffe.proto.caffe_pb2.Datum()
                     fc_fea.ParseFromString(fc_raw)
                     question.append( np.fromstring(fc_fea.data, dtype = np.float32))
-                    # Now, it is the inputs, we can use the other images other than current one.
-
-                    a = []
-                    a.append(self.vocab_size) # start token.
-                    a.extend(input_anno[1][2]) # this is the index for the captions.
-                    a_inp = np.zeros((max_ans_len, self.vocab_size + 1), dtype = floatX)
-                    a_mask = []
-                    for ans_idx, w_idx in enumerate(a):
-                        a_inp[ans_idx, w_idx] = 1
-
-                    a_mask = [ 1 for i in range(len(a) -1) ]
-                    while len(a) < max_ans_len: # this does not matter.
-                        a.append( -1 )
-                        a_mask.append(0)
-
-                    a = a[1:]
-                    answer.append(np.array(a).astype(np.int32))
-                    answer_mask.append(np.array(a_mask).astype(np.int32))
-                    answer_inp.append(np.array(a_inp).astype(floatX))
+                    answer.extend(input_anno[1][2]) # this is the index for the captions.
                 
-                #pdb.set_trace()
-                question = np.stack(question, axis = 0)
-                questions.append(question)
-                answer = np.stack(answer, axis = 0) # story_len x max_answer_len
-                answers.append(answer)
-                answer_mask = np.stack(answer_mask, axis =0) # story_len x max_answer_len -1
-                answers_mask.append(answer_mask)
-                answer_inp = np.stack(answer_inp, axis = 0) # story_len x max_answer_len
-                answers_inp.append(answer_inp)
+                answer_inp = np.zeros((max_ans_len, self.vocab_size + 1), dtype = floatX)
+                answer_mask = []
 
-        questions = np.stack(questions, axis = 0)
-        questions = np.array(questions).astype(floatX)
+                for ans_idx, w_idx in enumerate(answer):
+                    answer_inp[ans_idx, w_idx] = 1
+
+                answer_mask = [ 1 for i in range(len(answer) - 1)]
+                
+
+                while len(answer) < max_ans_len:
+                    answer.append(-1)
+                    answer_mask.append(0)
+                
+                answer = answer[1:]
+                answers.append( np.array(answer).astype(np.int32))
+                questions.append(np.stack(question, axis = 0))
+                answers_inp.append(answer_inp)
+                answers_mask.append(np.array(answer_mask).astype(np.int32))
+                
+        questions = np.stack(questions, axis = 0).astype(floatX)
         answers = np.array(answers).astype(np.int32)
         answers_mask = np.array(answers_mask).astype(floatX)
         answers_inp = np.stack(answers_inp, axis = 0)
-        questions = np.reshape(questions, (questions.shape[0] * questions.shape[1], questions.shape[2]))
-        answers = np.reshape(answers, (answers.shape[0] * answers.shape[1], answers.shape[2]))
-        answers_inp = np.reshape(answers_inp, (answers_inp.shape[0] * answers_inp.shape[1], answers_inp.shape[2], answers_inp.shape[3]))
-        answers_mask = np.reshape(answers_mask, (answers_mask.shape[0] * answers_mask.shape[1], answers_mask.shape[2]))
 
-        #print inputs.shape
         #print questions.shape
         #print answers.shape
         #print answers_inp.shape
         #print answers_mask.shape
 
 
-        return questions, answers, answers_inp, answers_mask
+        return questions, answers, answers_inp, answers_mask, stories
    
     
     def _process_batch(self, _inputs, _questions, _answers, _fact_counts, _input_masks):
@@ -470,6 +464,7 @@ class DMN_batch:
         for sid in dict_story:
             story = dict_story[sid].items()
             sorted(story, key = lambda x: x[0])
+            story = story[::-1]
             dict_story[sid] = story
 
         return dict_story, lmdb_env_fc
@@ -525,9 +520,9 @@ class DMN_batch:
         if mode == "test":    
             theano_fn = self.test_fn 
         
-        inp, q, ans, ans_inp, ans_mask = self._process_batch_sind(batch_index, mode)
+        q, ans, ans_inp, ans_mask, stories = self._process_batch_sind(batch_index, mode)
         
-        ret = theano_fn(inp, q, ans, ans_mask, ans_inp)
+        ret = theano_fn(q, ans, ans_mask, ans_inp)
         param_norm = np.max([utils.get_norm(x.get_value()) for x in self.params])
         
         return {"prediction": ret[0],
@@ -537,3 +532,100 @@ class DMN_batch:
                 "log": "pn: %.3f" % param_norm,
                 }
         
+    def step_beam(self, batch_index, beam_size = 10):
+        '''
+            This function is mainly for the testing stage.
+            Use the beam search to generate the target captions from each image.
+        '''
+
+        theano_fn = self.pred_fn
+        q, ans, ans_inp, ans_mask, img_ids = self._process_batch_sind(batch_index)
+        
+        batch_size = q.shape[0]
+
+        captions = []
+        batch_of_beams = [ [ (0.0, [self.vocab_size])] for i in range(batch_size) ]
+
+        nsteps = 0
+
+        while True:
+            logging.info('nsteps = %d', nsteps)
+            beam_c = [[] for i in range(batch_size) ]
+            idx_prevs = [ [] for i in range(batch_size)]
+            idx_of_idx = [[] for i in range(batch_size)]
+            idx_of_idx_len = [ ]
+
+            max_b = -1
+            cnt_ins = 0
+            for i in range(batch_size):
+                beams = batch_of_beams[i]
+                for k, b in enumerate(beams):
+                    idx_prev = b[-1]
+                    if idx_prev[-1] == self.vocab['.']:
+                        # This is the end.
+                        beam_c[i].append(b)
+                        continue
+
+                    idx_prevs[i].append( idx_prev )
+                    idx_of_idx[i].append(k)
+                    idx_of_idx_len.append( len(idx_prev))
+                    cnt_ins += 1
+
+                    if len(idx_prev) > max_b:
+                        max_b = len(idx_prev)
+
+            if cnt_ins == 0:
+                break
+            
+            x_i = np.zeros((cnt_ins, max_b, self.vocab_size + 1), dtype = 'float32')
+            q_i = np.zeros((cnt_ins, self.story_len, self.cnn_dim), dtype='float32')
+
+            idx_base = 0
+            for j,idx_prev_j in enumerate(idx_prevs):
+                for m, idx_prev in enumerate(idx_prev_j):
+                    for k in range(len(idx_prev)):
+                        x_i[m + idx_base,k,idx_prev[k]] = 1.0
+                q_i[idx_base:idx_base + len(idx_prev_j),:,:] = q[j,:,:]
+
+                idx_base += len(idx_prev_j)
+            # This is really pain full.
+            # Since the batch_size is fixed when creating the module. Thus,
+            # we need to make them equal to the batch_size.
+            pred = np.zeros_like(x_i)
+            for i in range(0, cnt_ins, batch_size):
+                start_idx = i
+                end_idx = i + batch_size
+                if end_idx > cnt_ins:
+                    end_idx = cnt_ins
+                    start_idx = end_idx - batch_size
+                t = theano_fn(q_i[start_idx:end_idx,:,:], x_i[start_idx:end_idx,:,:])
+                pred[start_idx:end_idx,:,:] = t[0]
+
+            p = np.zeros((pred.shape[0], pred.shape[2]))
+            for i in range(pred.shape[0]):
+                p[i,:] = pred[i,idx_of_idx_len[i]-1,:]
+
+            l = np.log( 1e-20 + p)
+            top_indices = np.argsort( -l, axis=-1)
+            idx_base = 0
+            for batch_i, idx_i in enumerate(idx_of_idx):
+                for j,idx in enumerate(idx_i):
+                    row_idx = idx_base + j
+                    for m in range(beam_size):
+                        wordix = top_indices[row_idx][m]
+                        beam_c[batch_i].append((batch_of_beams[batch_i][idx][0] + l[row_idx][wordix], batch_of_beams[batch_i][idx][1] + [wordix]))
+                idx_base += len(idx_i)
+
+            for i in range(len(beam_c)):
+                beam_c[i].sort(reverse = True) # descreasing order.
+            for i, b in enumerate(beam_c):
+                batch_of_beams[i] = beam_c[i][:beam_size]
+            nsteps += 1
+            if nsteps >= 20:
+                break
+        for beams in batch_of_beams:
+            pred = [(b[0], b[1]) for b in beams ]
+            captions.append(pred)
+
+        return {'captions':captions,
+                'img_ids': img_ids}

show_and_tell_sind_lasagne.py
@@ -116,13 +116,20 @@ class DMN_batch:
             cost = T.nnet.categorical_crossentropy(preds, targets)[T.flatten(mask).nonzero()]
             return cost
         
-        loss = T.mean(calc_cross_ent(output, self.answer_mask, self.answer_var))
-        
+        self.loss_ce = T.mean(calc_cross_ent(output, self.answer_mask, self.answer_var))
+ 
         MAX_GRAD_NORM = 1e5
         
         self.params = lasagne.layers.get_all_params(l_out, trainable=True)
+
+        if self.l2 > 0:
+            self.loss_l2 = self.l2 * nn_utils.l2_reg(self.params)
+        else:
+            self.loss_l2 = 0
         
-        all_grads = T.grad(loss, self.params)
+        self.loss = self.loss_ce + self.loss_l2
+       
+        all_grads = T.grad(self.loss, self.params)
         all_grads = [T.clip(g, -5, 5) for g in all_grads]
         all_grads, norm = lasagne.updates.total_norm_constraint(
             all_grads, MAX_GRAD_NORM, return_norm=True)
@@ -130,11 +137,11 @@ class DMN_batch:
         updates = lasagne.updates.adam(all_grads, self.params, learning_rate=0.001)
         
         self.train_fn = theano.function([self.q_var, self.answer_var, self.answer_mask, self.answer_inp_var],
-                                  [loss, output, norm],
+                                  [self.loss, output, norm],
                                   updates=updates
                                  )
         
-        self.test_fn = theano.function([self.q_var, self.answer_var, self.answer_mask, self.answer_inp_var], [loss, output, norm])
+        self.test_fn = theano.function([self.q_var, self.answer_var, self.answer_mask, self.answer_inp_var], [self.loss, output, norm])
 
         self.pred_fn = theano.function([self.q_var, self.answer_inp_var], [output])
     def _empty_word_vector(self):
@@ -367,6 +374,11 @@ class DMN_batch:
         # Now, add UNK
         vocab['UNK'] = len(vocab)
         ivocab[len(ivocab)] = 'UNK'
+        vocab['[male]'] = len(vocab)
+        ivocab[len(ivocab)] = '[male]'
+        vocab['[female]'] = len(vocab)
+        ivocab[len(ivocab)] = '[female]'
+
 
         logging.info('len(vocab) / len(ivocab) = %d/%d', len(vocab), len(ivocab))
         return vocab, ivocab
@@ -459,21 +471,21 @@ class DMN_batch:
             if cnt_ins == 0:
                 break
             
-            x_i = np.zeros((cnt_ins, max_b, self.vocab_size + 1), dtype = 'float32')
+            x_i = np.zeros((cnt_ins, self.SEQUENCE_LENGTH-1), dtype = 'int32')
             q_i = np.zeros((cnt_ins, self.cnn_dim), dtype='float32')
 
             idx_base = 0
             for j,idx_prev_j in enumerate(idx_prevs):
                 for m, idx_prev in enumerate(idx_prev_j):
                     for k in range(len(idx_prev)):
-                        x_i[m + idx_base,k,idx_prev[k]] = 1.0
+                        x_i[m + idx_base,k] = idx_prev[k]
                 q_i[idx_base:idx_base + len(idx_prev_j),:] = q[j,:]
 
                 idx_base += len(idx_prev_j)
             # This is really pain full.
             # Since the batch_size is fixed when creating the module. Thus,
             # we need to make them equal to the batch_size.
-            pred = np.zeros_like(x_i)
+            pred = np.zeros((cnt_ins, x_i.shape[1]), dtype = 'float32')
             for i in range(0, x_i.shape[0], batch_size):
                 start_idx = i
                 end_idx = i + batch_size
@@ -481,8 +493,8 @@ class DMN_batch:
                     end_idx = x_i.shape[0]
                     start_idx = end_idx - batch_size
                 
-                t = theano_fn(q_i[start_idx:end_idx,:], x_i[start_idx:end_idx,:,:])
-                pred[start_idx:end_idx,:,:] = t[0]
+                t = theano_fn(q_i[start_idx:end_idx,:], x_i[start_idx:end_idx,:])
+                pred[start_idx:end_idx,:] = t[0]
 
             p = np.zeros((pred.shape[0], pred.shape[2]))
             for i in range(pred.shape[0]):

test_theano.py
@@ -4,74 +4,29 @@ import numpy as np
 import nn_utils
 
 input_var = T.tensor3('input_var') # (batch_size, seq_len, cnn_dim)
-dim = 40
-cnn_dim = 4096
 
-W_inp_emb_in = nn_utils.normal_param(std=0.1, shape=(dim, cnn_dim))
-b_inp_emb_in = nn_utils.constant_param(value=0.0, shape=(dim,))
+input_var_rhp = T.reshape(input_var, (input_var.shape[0] * input_var.shape[1], input_var.shape[2]))
 
-inp_var_shuffled = input_var.dimshuffle(1,2,0)
+prob = nn_utils.softmax(input_var_rhp)
+prob_rhp = T.reshape(prob, (input_var.shape[0], input_var.shape[1], input_var.shape[2]))
 
-def _dot(x, W, b):
-    return  T.dot(W, x) + b.dimshuffle(0, 'x')
+sf = theano.function(inputs = [input_var], outputs = prob_rhp)
 
-inp_c_hist,_ = theano.scan(fn = _dot, sequences=inp_var_shuffled, non_sequences = [W_inp_emb_in, b_inp_emb_in])
+inp = np.random.rand(4,5,3).astype('float32')
 
-f = theano.function(inputs = [input_var], outputs = inp_c_hist)
+t = sf(inp)
 
-inp = np.random.rand(10,4,4096).astype('float32')
+print 't', t
+print 'sum(2)', t.sum(2)
+print 'sum(1)', t.sum(1)
+print 'sum(0)', t.sum(0)
 
-t = f(inp)
-print t.shape
- 
-#k = T.iscalar("k")
-#A = T.vector("A")
-#
-## Symbolic description of the result
-#result, updates = theano.scan(fn=lambda prior_result, A: prior_result * A,
-#                                      outputs_info=T.ones_like(A),
-#                                                                    non_sequences=A,
-#                                                                                                  n_steps=k)
-#
-## We only care about A**k, but scan has provided us with A**1 through A**k.
-## Discard the values that we don't care about. Scan is smart enough to
-## notice this and not waste memory saving them.
-#final_result = result
-#
-## compiled function that returns A**k
-#power = theano.function(inputs=[A,k], outputs=final_result, updates=updates)
+print
 
-#print(power(range(10),2))
-#print(power(range(10),4))
+t2 = np.reshape(t, (t.shape[0] * t.shape[1], t.shape[2]))
 
+print 't2, sum(0)'
+print t2.sum(0)
 
-k = T.tensor3("K")
-
-k2 = k.dimshuffle(1,2,0)
-
-w = T.matrix('w')
-
-m = T.dot(k, w)
-
-t = T.tensor3('t')
-
-mu = theano.function(inputs = [k, w], outputs = m)
-
-def mu(a,b):
-    return T.dot(b,a)
-
-t,_ = theano.scan(fn = mu, sequences = [k2], non_sequences =[ w], outputs_info=None)
-
-mu2 = theano.function(inputs = [k,w], outputs = t)
-
-a = np.random.rand(4,3,10)
-print a.flags
-
-a = np.array(a, dtype = 'float32')
-b = np.random.rand(5,10)
-
-b= np.array(b, dtype= 'float32')
-
-print mu2(a, b).shape
-
-
+print 't2, sum(1)'
+print t2.sum(1)